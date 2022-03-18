38d9e8036f5d0dec2c9296b60a6426eafc11960f
Terzea Bogdan <terzeabogdan2007@yahoo.com>
2022-03-18 14:57:08 +0200

    Add buy tokens option

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,7 +6,8 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  uint256 public constant tokensPerEth = 100;
 
   YourToken public yourToken;
 
@@ -15,6 +16,11 @@ contract Vendor is Ownable {
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    uint256 amount = msg.value * tokensPerEth;
+    yourToken.transfer(msg.sender, amount);
+    emit BuyTokens(msg.sender, msg.value, amount);
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
